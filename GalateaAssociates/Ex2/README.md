e4131b25b15dcec9dcd533df6dddc9f445d97878
Ben Cao <benjamin.cao88@gmail.com>
2017-09-05 13:31:31 -0400

    Galatea Ex2 Commit

---
+++ GalateaAssociates/Ex2/README.md
@@ -0,0 +1,3 @@
+# Task 2
+
+![Task2](https://github.com/caofontaine/CodingExercises/blob/master/GalateaAssociates/ex2s/img/task2.png "Task2")
---
+++ GalateaAssociates/Ex2/ex2.java
@@ -0,0 +1,5 @@
+class Solution {
+  public int solution(int V, int R) {
+    // Returns a string representation of V in radix R.
+    return Integer.toString(V, R);
+}
